8da2e2e7da7fdaf501164727fb78570a40c39dde
Muhammad Firas <112752858+firas-07@users.noreply.github.com>
2024-12-01 09:45:04 +0530

    Delete backend directory

--- backend/src/main/java/com/firas/server/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.firas.server.repository;
-
-import com.firas.server.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
