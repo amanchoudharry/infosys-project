b1755bccd82242882629f3dd8d947160aba676de
Muhammad Firas <112752858+firas-07@users.noreply.github.com>
2024-12-04 21:52:27 +0530

    Delete backend directory

--- backend/src/main/java/com/firas/server/repository/ADHDTestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firas.server.repository;
-
-import com.firas.server.model.ADHDTest;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ADHDTestRepository extends MongoRepository<ADHDTest, String> {
-}
--- backend/src/main/java/com/firas/server/repository/BipolarTestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firas.server.repository;
-
-import com.firas.server.model.BipolarTest;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BipolarTestRepository extends MongoRepository<BipolarTest, String> {
-}
