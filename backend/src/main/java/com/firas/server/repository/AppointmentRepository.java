a4b17c15163498a46bc37d0e2dc82d7bfa039b19
Muhammad Firas <112752858+firas-07@users.noreply.github.com>
2024-12-19 19:05:13 +0530

    Delete backend directory

--- backend/src/main/java/com/firas/server/repository/AppointmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firas.server.repository;
-
-import com.firas.server.model.Appointment;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AppointmentRepository extends MongoRepository<Appointment, String> {
-}
--- backend/src/main/java/com/firas/server/repository/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.firas.server.repository;
-
-import com.firas.server.model.ServiceRequest;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ServiceRepository extends MongoRepository<ServiceRequest, String> {
-}
